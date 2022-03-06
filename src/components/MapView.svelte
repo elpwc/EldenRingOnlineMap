819e93b16d0ede30cd3e42648ca54246c21d35ea
elpwc/wniko <elpwc@hotmail.com>
2022-03-06 19:13:05 +0800

    feat: 变更了退出编辑模式的按钮的样式

--- src/components/MapView.svelte
+++ src/components/MapView.svelte
@@ -658,9 +658,15 @@
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
           <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
         </svg>
+      {:else}
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+          <path
+            d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"
+          />
+        </svg>
       {/if}
       <p>
-        {isAddPointMode ? '返回地图' : '添加地标'}
+        {isAddPointMode ? '退出编辑' : '添加地标'}
       </p>
     </button>
   </header>
