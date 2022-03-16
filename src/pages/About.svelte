229c74f144f3c53b9ecb04c5b5cfd0e8b0685884
elpwc/wniko <elpwc@hotmail.com>
2022-03-16 19:12:46 +0800

    update: 2.2.0.0

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -93,7 +93,7 @@
         </svg>
       </sup>
     </a><br />
-    {Lang.about.lastUpdate}: 2020-3-15 13:44
+    {Lang.about.lastUpdate}: 2020-3-16 19:10
     <br />
     Contributors:
     <a href="https://github.com/spking11" target="_blank"
