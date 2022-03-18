c068bfb6af8ef4cf0da0f55d23ce132963285e51
elpwc/wniko <elpwc@hotmail.com>
2022-03-18 17:29:46 +0800

    feat: 将旧cookie中的Lang转移到localstorage

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@
 import axios from 'axios';
 import App from './App.svelte';
 import Config from './config';
-import { set_client_ip } from './utils/utils';
+import { getCookie, setCookie, set_client_ip } from './utils/utils';
 import './common';
-import { setupI18n } from './locale'
+import { setupI18n } from './locale';
 import { setUpConvertor } from './utils/convertor';
 
 // 设置api根目录
@@ -19,9 +19,17 @@ try {
   console.log(e);
 }
 
+// 旧cookie中的lang导入localStorage
+if (getCookie('lang') !== '') {
+  localStorage.setItem('lang', getCookie('lang'));
+  setCookie('lang', '', 0);
+}
+
+// 设置语言
 setupI18n();
 setUpConvertor();
 
+// 启动
 const app = new App({
   target: document.body,
   props: {},
