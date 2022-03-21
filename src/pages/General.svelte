5af1dbebe1cafbe43192d7f3ee954f76abb2ef4a
elpwc/wniko <elpwc@hotmail.com>
2022-03-21 19:21:35 +0800

    ui: 為設置頁安排更好的pc適配

--- src/pages/General.svelte
+++ src/pages/General.svelte
@@ -4,40 +4,48 @@
   import { t } from 'svelte-i18n';
   import { changeLang, lang } from '../locale';
   import { changeConvertTarget, ConvertType } from '../utils/convertor';
-  import { convertTargetStore } from '../stores';
+  import { convertTargetStore, isMobile } from '../stores';
 </script>
 
-<div class="container">
-  <div class="btnContainer">
+<div class="container" style="width: {isMobile ? '100%' : '50%'};">
+  <div class="settingItem">
     <p>{$t('general.menulang')}</p>
-    <LangButton buttonLang={SupportedLang.zhCN} buttonText="简体" currentLang={$lang} on:click={event => changeLang(event.detail.lang)} />
-    <LangButton buttonLang={SupportedLang.zhTW} buttonText="正體" currentLang={$lang} on:click={event => changeLang(event.detail.lang)} />
+    <div class="btnContainer">
+      <LangButton buttonLang={SupportedLang.zhCN} buttonText="简体" currentLang={$lang} on:click={event => changeLang(event.detail.lang)} />
+      <LangButton buttonLang={SupportedLang.zhTW} buttonText="正體" currentLang={$lang} on:click={event => changeLang(event.detail.lang)} />
+    </div>
   </div>
 
   <br />
-  <div class="btnContainer">
+
+  <div class="settingItem">
     <p>{$t('general.maplang')}</p>
-    <LangButton buttonLang={ConvertType.s2t} buttonText="简转繁" currentLang={$convertTargetStore} on:click={event => changeConvertTarget(event.detail.lang)} />
-    <LangButton buttonLang={ConvertType.t2s} buttonText="繁轉簡" currentLang={$convertTargetStore} on:click={event => changeConvertTarget(event.detail.lang)} />
-    <LangButton buttonLang={ConvertType.dont} buttonText={$t('general.dontConvert')} currentLang={$convertTargetStore} on:click={event => changeConvertTarget(event.detail.lang)} />
+    <div class="btnContainer">
+      <LangButton buttonLang={ConvertType.s2t} buttonText="简转繁" currentLang={$convertTargetStore} on:click={event => changeConvertTarget(event.detail.lang)} />
+      <LangButton buttonLang={ConvertType.t2s} buttonText="繁轉簡" currentLang={$convertTargetStore} on:click={event => changeConvertTarget(event.detail.lang)} />
+      <LangButton buttonLang={ConvertType.dont} buttonText={$t('general.dontConvert')} currentLang={$convertTargetStore} on:click={event => changeConvertTarget(event.detail.lang)} />
+    </div>
   </div>
 
   <br />
 
-  <button class="settingbutton"
-    on:click={() => {
-      const hiddens = localStorage.getItem('hidden')?.split('|');
-      if (!hiddens || hiddens.length === 0) {
-        alert($t('general.theresNoHiddenPointByNow'));
-      } else {
-        const r = confirm($t('general.therereSomeHiddenPoints').replace('{count}', (hiddens.length-1).toString()));
-        if (r === true) {
-          localStorage.setItem('hidden', '');
-          alert($t('general.hiddenPointYattaze'));
+  <div class="settingItem">
+    <button
+      class="settingbutton"
+      on:click={() => {
+        const hiddens = localStorage.getItem('hidden')?.split('|');
+        if (!hiddens || hiddens.length === 0) {
+          alert($t('general.theresNoHiddenPointByNow'));
+        } else {
+          const r = confirm($t('general.therereSomeHiddenPoints').replace('{count}', (hiddens.length - 1).toString()));
+          if (r === true) {
+            localStorage.setItem('hidden', '');
+            alert($t('general.hiddenPointYattaze'));
+          }
         }
-      }
-    }}>⚠ {$t('general.clearHiddenPointButton')} ⚠</button
-  >
+      }}>⚠ {$t('general.clearHiddenPointButton')} ⚠</button
+    >
+  </div>
 
   <p>{$t('general.developing')}</p>
 </div>
@@ -45,21 +53,28 @@
 <style>
   .container {
     height: 100%;
-    width: 100%;
     padding: 5px;
+    margin: auto;
   }
   .container p {
     color: rgb(208, 200, 181);
     min-width: fit-content;
     margin-right: 10px;
   }
-  .btnContainer {
+  .settingItem {
     display: flex;
+    justify-content: space-between;
+    align-items: center;
     width: 90%;
     padding: 0 10px;
   }
-  .settingbutton{
-    width: 90%;
+  .btnContainer {
+    display: flex;
+    width: 70%;
+  }
+  .settingbutton {
+    width: 100%;
     padding: 10px;
+    font-size: 1em;
   }
 </style>
