f3064656ab4b6fb2525cf61aa63a40b228ea717e
elpwc/wniko <elpwc@hotmail.com>
2022-03-21 12:33:59 +0800

    ui: 添加支线页说明

--- src/pages/Routes.svelte
+++ src/pages/Routes.svelte
@@ -3,14 +3,19 @@
 </script>
 
 <div class="container">
-  <p>开发中</p>
+  <p>开发中, 预计功能：</p>
+  <p>1. 在地图上标注路线</p>
+  <p>2. 专门的支线说明页</p>
+  <br/>
+  <p>尽量一个月内完成吧</p>
+  <img src="https://www.elpwc.com/imagebank/bakaqua.gif" width="100px" alt="aqua"/>
 </div>
 
 <style>
   .container {
-    display: flex;
     height: 100%;
     width: 100%;
+    text-align: center;
   }
   .container p {
     color: rgb(208, 200, 181);
