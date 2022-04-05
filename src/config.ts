064770d7faf9af3a620eb7c1f8dd7b131027c09a
elpwc/wniko <elpwc@hotmail.com>
2022-04-05 16:51:19 +0800

    update: 3.1.5.0

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,8 @@
  */
 const Config = {
   APIBaseURL: './api/',
-  currentVer: '3.1.4.0',
-  lastUpdated: '2022-4-5 16:26'
+  currentVer: '3.1.5.0',
+  lastUpdated: '2022-4-5 16:50'
 };
 
 export default Config;
