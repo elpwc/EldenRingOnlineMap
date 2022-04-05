606dc1d789a19d3a52d88d64f3a602e42c788a54
elpwc/wniko <elpwc@hotmail.com>
2022-04-05 15:51:55 +0800

    debug: route

--- src/components/RouteViewer/RouteViewer.svelte
+++ src/components/RouteViewer/RouteViewer.svelte
@@ -1,24 +1,64 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
+  import { afterUpdate, onMount } from 'svelte';
 
   import { Box, BoxTypes } from './types';
   import { dagStratify, sugiyama, DagNode, zherebko, grid } from 'd3-dag';
   import Drawer from './drawer';
   import { boxes } from './data';
 
+  let range = { width: 0, height: 0 };
+  let data;
+
+  let rooms: { room: { data; lines: { x: number; y: number }[]; x; y } }[][] = [];
+
+  const getRooms = () => {
+      
+  }
+
   onMount(() => {
-    const data = Drawer.d3DagStratify(Drawer.getTree(boxes));
-    const canvas = document.getElementById('routeCanvas') as HTMLCanvasElement;
-    Drawer.draw(data.result, boxes, canvas);
+    data = Drawer.d3DagStratify(Drawer.getTree(boxes));
+
+    range = data.size;
+
+    // const canvas = document.getElementById('routeCanvas') as HTMLCanvasElement;
+  });
+
+  afterUpdate(() => {
+    const container = document.getElementById('rowContainer');
+    Drawer.drawDOM(data.result, boxes, container, 100, 50);
   });
 </script>
 
 <div>
-  <canvas id="routeCanvas" width="500px" height="1000px">你的浏览器不支持Canvas</canvas>
+  <!--canvas id="routeCanvas" width="500px" height="1000px">你的浏览器不支持Canvas</canvas-->
+  <div id="rowContainer">
+    {#each new Array(range.height * 2) as h, j}
+      <div class="row row{j}">
+        {#each new Array(range.width * 2) as w, i}
+          <div class="column column{i} row{j}">
+            <p>{i}{j}</p>
+          </div>
+        {/each}
+      </div>
+    {/each}
+  </div>
 </div>
 
 <style>
-    canvas{
-        background-color: rgb(21, 22, 17);
-    }
+  canvas {
+    background-color: rgb(21, 22, 17);
+  }
+  #rowContainer {
+    display: flex;
+    flex-direction: column;
+  }
+  .row {
+    display: flex;
+  }
+  .column {
+    width: -webkit-fill-available;
+    height: 50px;
+    border: solid 1px white;
+    color: white;
+  }
 </style>
--- src/components/RouteViewer/drawer.ts
+++ src/components/RouteViewer/drawer.ts
@@ -3,6 +3,10 @@ import { dagStratify, sugiyama, DagNode, zherebko, grid } from 'd3-dag';
 import { BoxStyles } from './styles';
 
 export default class Drawer {
+    public static currentClickedBoxId: number = 1;
+
+    public static mappedBoxes: {boxdata, range: [number, number, number, number]}[];
+
   /**
    * 将自定义DAG格式转化为D3-DAG格式
    * @param boxes
@@ -33,6 +37,13 @@ export default class Drawer {
     return { result: dag, size: sugiyamainfo };
   };
 
+  public static refreshMappedBoxes = (stratifiedData) => {
+
+    const getRange = (node) => {
+        
+    }
+  }
+
   /**
    * 画！（一个节点和他的子节点
    * @param node 节点
@@ -41,7 +52,7 @@ export default class Drawer {
    * @param scaleX X方向拉伸
    * @param scaleY Y方向拉伸
    */
-  public static draw = (node, boxes: Box[], canvas: HTMLCanvasElement, scaleX: number = 50, scaleY: number = 50) => {
+  public static drawCanvas = (node, boxes: Box[], canvas: HTMLCanvasElement, scaleX: number = 50, scaleY: number = 50) => {
     const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
 
     // 获取box实例
@@ -77,7 +88,67 @@ export default class Drawer {
       });
 
       // 递归到子节点
-      this.draw(child?.child, boxes, canvas, scaleX, scaleY);
+      this.drawCanvas(child?.child, boxes, canvas, scaleX, scaleY);
     });
   };
+
+    /**
+   * 画！（一个节点和他的子节点
+   * @param node 节点
+   * @param boxes 所有box
+   * @param canvas canvasDOM
+   * @param scaleX X方向拉伸
+   * @param scaleY Y方向拉伸
+   */
+     public static drawDOM = (node, boxes: Box[], container: HTMLElement, scaleX: number = 50, scaleY: number = 50) => {
+
+      // 获取box实例
+      const instance = boxes.filter(f => {
+        return f.id === Number(node.data.id);
+      })?.[0];
+  
+      // 获取style
+      const boxStyle = BoxStyles.filter(f => {
+        return f.type === instance.type;
+      })?.[0];
+  
+      // 获取目标DOM
+      console.log(`column${node.x*2} row${node.y*2}`, container.getElementsByClassName(`column${node.x*2} row${node.y*2}`))
+
+      const rooms = container.getElementsByClassName(`column${node.x*2} row${node.y*2}`);
+      console.log(rooms, rooms.item.length, rooms[0]);
+      const room = rooms[0];
+      if(room){
+
+        room.innerHTML=`
+        <p style="color: red;">123</p>
+        `
+        console.log(room, room.innerHTML)
+        // // 画图标
+        // ctx.beginPath();
+        // ctx.arc(node.x * scaleX, node.y * scaleY, 5, 0, 2 * Math.PI);
+        // ctx.stroke();
+    
+        // // 设置字体
+        // ctx.font = boxStyle.font;
+        // ctx.fillStyle = boxStyle.color ?? 'white';
+        // // 画文字
+        // ctx.fillText(instance.name, node.x * scaleX + 5, node.y * scaleY);
+    
+        // 画子节点的线
+        node?.dataChildren.forEach(child => {
+          // child?.points.forEach((p, i) => {
+          //   if (i > 0) {
+          //     ctx.moveTo(child?.points[i - 1].x * scaleX, child?.points[i - 1].y * scaleY);
+          //     ctx.lineTo(p.x * scaleX, p.y * scaleY);
+          //     ctx.strokeStyle = 'white';
+          //     ctx.stroke();
+          //   }
+          // });
+    
+          // 递归到子节点
+          this.drawDOM(child?.child, boxes, container, scaleX, scaleY);
+        });
+      }
+    };
 }
