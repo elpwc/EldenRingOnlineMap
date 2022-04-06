f7090175e42c44caf56a5efeb80f7fbf4da091be
elpwc/wniko <elpwc@hotmail.com>
2022-04-06 11:38:43 +0800

    update: 3.1.5.1

--- src/App.svelte
+++ src/App.svelte
@@ -52,7 +52,9 @@
   <Modal
     visible={updateVisibility}
     showOkButton
-    top="10%"
+    top="0%"
+    width="85%"
+    title="更新内容"
     zindex={1919811}
     onOKButtonClick={() => {
       updateVisibility = false;
@@ -62,6 +64,20 @@
     <div id="modalContainer">
       <div class="update">
         <p class="updateTitle"><date>{config.default.lastUpdated}</date> 的更新<br />(ver {config.default.currentVer})</p>
+        <div class="updateSection">
+          <p class="sectionTitle">💡 新的<ruby>特性</ruby>:</p>
+          <p>- 添加了3DMap页的基底（开发中）</p>
+          <p>- 添加了支线页的预览(?)（即将完成）</p>
+          <p>- 优化了对话框的排版, 按钮不会被挤到下面了</p>
+        </div>
+        <div class="updateSection">
+          <p class="sectionTitle">🐞 修复了bug:</p>
+          <p>- 服务器响应失败时说明页会卡住</p>
+        </div>
+      </div>
+      <hr />
+      <div class="update">
+        <p class="updateTitle"><date>2022-4-5 16:50</date> 的更新<br />(ver 3.1.5.0)</p>
         <div class="updateSection">
           <p class="sectionTitle">💡 新的<ruby>特性<rt>bug</rt></ruby>:</p>
           <p>- 添加了开屏更新提示</p>
@@ -108,6 +124,7 @@
     display: flex;
     flex-direction: column;
     gap: 10px;
+    width: -webkit-fill-available;
   }
   .updateSection {
     text-align: left;
@@ -119,5 +136,9 @@
   .sectionTitle {
     text-shadow: 0 0 2px rgb(209, 207, 198);
     font-weight: bold;
+  }
+  hr {
+    width: -webkit-fill-available;
+    background-color: navajowhite;
   }
 </style>
--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,8 @@
  */
 const Config = {
   APIBaseURL: './api/',
-  currentVer: '3.1.5.0',
-  lastUpdated: '2022-4-5 16:50'
+  currentVer: '3.1.5.1',
+  lastUpdated: '2022-4-6 11:37',
 };
 
 export default Config;
