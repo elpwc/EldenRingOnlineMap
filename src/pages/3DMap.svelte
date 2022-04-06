4c6b1847c6cec3cc2d1fec399db2c0cf0c17bfb7
elpwc/wniko <elpwc@hotmail.com>
2022-04-06 11:06:03 +0800

    feat: 添加3d地图预览

--- src/pages/3DMap.svelte
+++ src/pages/3DMap.svelte
@@ -5,10 +5,11 @@
 <div class="container">
   <p>得到了<a href="https://space.bilibili.com/873348" target="_blank">@纳闷虎</a>先生的授权</p>
   <p>会试着将他精心制作的各区域3D地图以在线可互动(但不可编辑)的形式呈现在这里</p>
+  <img src="./resource/images/3dmappreview.png" width="300px" alt="3dmap preview" />
   <br />
   <p>做完支线就开工这个（大概）</p>
-  <img src="https://www.elpwc.com/imagebank/bakaqua.gif" width="100px" alt="aqua" />
   <p>（或者说，有来一起开发的老哥吗（）</p>
+  <img src="https://www.elpwc.com/imagebank/bakaqua.gif" width="100px" alt="aqua" />
 </div>
 
 <style>
