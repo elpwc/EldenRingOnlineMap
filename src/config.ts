b053fa64cab3c47b9bb40747147e9773fb9242e2
elpwc/wniko <elpwc@hotmail.com>
2022-04-30 02:12:18 +0800

    update: 3.3.0.1

--- src/config.ts
+++ src/config.ts
@@ -4,8 +4,8 @@
  */
 const Config = {
   APIBaseURL: './api/',
-  currentVer: '3.3.0.1', // 本来想着稳定到3.1415926的，果然还是跳过了阿...
-  lastUpdated: '2022-4-30 01:56',
+  currentVer: '3.3.0.2', // 本来想着稳定到3.1415926的，果然还是跳过了阿...
+  lastUpdated: '2022-4-30 02:07',
   /** 是否锁定所有地标 */
   isLockAllMarkers: false,
   inDev: false,
